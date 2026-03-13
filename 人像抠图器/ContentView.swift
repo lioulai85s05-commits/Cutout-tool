bfa08ee8a6aa33445c3da1bcb9c74f573fd048ee
lioulai85s05-commits <lioula85s05@fmail.com>
2026-03-13 20:38:29 +0800

    Initial Commit

---
+++ 人像抠图器/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  人像抠图器
+//
+//  Created by Snake on 2026/3/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
